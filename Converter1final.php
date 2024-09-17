b1ac6f46a4b5cf1d3d5892ffac74f989f164df2f
Yukimuraa <91885768+Yukimuraa@users.noreply.github.com>
2024-09-17 13:26:51 +0800

    first push

--- Converter1final.php
+++ Converter1final.php
@@ -11,14 +11,14 @@
         if(isset($_POST["submit"])){
         $Celsuis = floatval($_POST["celsuis"]);
         $Farenheit = $Celsuis * 9 / 5 + 32;
-    }
+        }
         ?>
 
         <form action="Converter1final.php" method="post" class="box">
             <h1>PHP Converter</h1>
             <hr>
             <input type="text" name="celsuis" placeholder="Enter Celsuis" required>
-            <input type="text" name="farenheit" value="<?php echo $Farenheit;?>" placeholder="Farenheit" readonly>
+            <input type="text" name="farenheit" value="<?php echo $Farenheit;?>" placeholder="Farenheit">
             <input type="submit" name="submit" value="Calculate" class="sub">
         </form>
     </body>
